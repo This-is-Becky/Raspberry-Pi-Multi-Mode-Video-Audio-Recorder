2f45f508e36c6390dce3c42aaeb80e676385f3d1
This-is-Becky <165574421+This-is-Becky@users.noreply.github.com>
2025-10-03 14:32:12 +0800

    Add files via upload

---
+++ Execute_code.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# -------------------------------
+# Raspberry Pi Recording Launcher
+# -------------------------------
+
+# Activate Python environment (if using virtualenv or venv)
+# source /home/pi/myenv/bin/activate
+
+# Navigate to the project directory
+cd /home/pi/Documents
+
+# Run the main Python script
+python3 Various_record.py
+
+# End of script
